e0dd252442ea8b62bdb2ea7baf0fb93d4427de27
felixojiambo <felixojiamboe@gmail.com>
2024-11-29 23:30:51 +0300

    Add HashUtils for SHA-1 hashing functionality

--- src/main/java/com/pesapal/felixvcs/utils/HashUtils.java
+++ src/main/java/com/pesapal/felixvcs/utils/HashUtils.java
@@ -1,4 +1,24 @@
 package com.pesapal.felixvcs.utils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashUtils {
+    public static String sha1(byte[] data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hashBytes = digest.digest(data);
+            return bytesToHex(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-1 algorithm not found.");
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for(byte b: bytes){
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
 }
